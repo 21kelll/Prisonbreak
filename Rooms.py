7ce7834938d83bb255b5b857bd09f638eed2d477
20mitchells <57879044+20mitchells@users.noreply.github.com>
2019-11-18 09:19:32 +0900

    Update Rooms.py

--- Rooms.py
+++ Rooms.py
@@ -38,7 +38,11 @@ room = ["description", 9, None, 10, 5]
 room_list.append(room)
 
 # -- yard (9)
-room = ["description", None, None, 8, None]
+room = ["description", 12, None, 8, None]
+room_list.append(room)
+
+# -- alpha (12)
+room = ["description", None, None, 9, None]
 room_list.append(room)
 
 # -- exit (10)
@@ -48,3 +52,50 @@ room_list.append(room)
 # -- freedom (11)
 room = ["description"]
 room_list.append(room)
+
+# -- Starting point
+current_room = 0
+
+# -- Main Loop
+finished = False
+
+while not finished:
+    print(room_list[current_room][0])
+    userInput = input("Which way?: ")
+
+    # -- Quit
+    if userInput.lower() == "q":
+        print("Thanks for playing! Better luck next time.")
+        break
+        
+    # -- North
+    elif userInput.lower() == "n":
+        next_room = current_room[1]
+        if next_room is None:
+            print("You can't go that way.")
+        else:
+            current_room = next_room
+        
+    # -- East
+    elif userInput.lower() == "e":
+        next_room = current_room[2]
+        if next_room is None:
+            print("You can't go that way.")
+        else:
+            current_room = next_room
+
+    # -- South
+    elif userInput.lower() == "s":
+        next_room = current_room[3]
+        if next_room is None:
+            print("You can't go that way.")
+        else:
+            current_room = next_room
+
+        # -- West
+    elif userInput.lower() == "w":
+        next_room = current_room[4]
+        if next_room is None:
+            print("You can't go that way.")
+        else:
+            current_room = next_room
